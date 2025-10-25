4697d427e261b5fe1d8ff63ec3b954007f7897d7
Labib Mina <labibminaapps@gmail.com>
2025-10-25 10:15:50 -0700

    build script for project

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+g++ -static -mwindows \
+    -IC:/wxWidgets/include \
+    -IC:/wxWidgets/build-static/lib/gcc_x64_lib/mswu \
+    -LC:/wxWidgets/build-static/lib/gcc_x64_lib \
+    -o test.exe test.cpp \
+    -Wl,--start-group \
+        -lwxmsw32ud \
+        -lwxpngd \
+        -lwxzlibd \
+        -lole32 -loleaut32 -lcomctl32 -lcomdlg32 -luuid \
+        -lgdi32 -lshell32 -ladvapi32 -lversion \
+        -lshlwapi -luxtheme -loleacc -lwinspool \
+    -Wl,--end-group
